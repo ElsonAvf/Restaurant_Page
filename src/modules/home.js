3fc6f61ba0e251335d1250d7c5771b2355e25bcf
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-02-06 18:52:40 -0300

    Create 2 new introductionMessage
    To put each message on different p tags

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,24 +1,39 @@
 import pizzaSliceImage from './../assets/images/pizza-slice-icon.png';
+import './../assets/css/home.css';
 
 const homeDiv = document.createElement('div');
 const pizzaSliceIcon = document.createElement('img');
-const introductionMessage = document.createElement('p');
+const introductionMessage1 = document.createElement('p');
+const introductionMessage2 = document.createElement('p');
+const introductionMessage3 = document.createElement('p');
 const welcomeMessage = document.createElement('p');
 const restaurantName = document.createElement('p');
 
+//Add Values
+homeDiv.classList.add('homeDiv');
+
 pizzaSliceIcon.src = pizzaSliceImage;
-introductionMessage.textContent = 'We have the best pizza in the country.\nAll you need is our pizza!'
-welcomeMessage.textContent = 'You have tried the rest, now taste the BEST!\nWelcome to'
+
+// Add Text
+introductionMessage1.textContent = 'You have tried the rest, now taste the BEST!';
+introductionMessage2.textContent = 'All you need is our pizza!';
+introductionMessage3.textContent = 'We have the best pizza in the country.';
+
+
+welcomeMessage.textContent = 'Welcome to';
+
 restaurantName.textContent = 'La Pizza';
 
 homeDiv.append(
   pizzaSliceIcon,
-  introductionMessage,
+  introductionMessage1,
+  introductionMessage2,
+  introductionMessage3,
   welcomeMessage,
   restaurantName,
-  )
+);
   
-  export {homeDiv};
+export {homeDiv};
 
 
 
