e3049d514f35dec329ab510437ce86b91ddb0213
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-02-07 08:28:21 -0300

    Create the initial of the contact page

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -7,15 +7,21 @@ import adress from './../assets/icons/map-marker-outline.svg';
 import './../assets/css/contact.css';
 
 const contactDiv = document.createElement('div');
+contactDiv.id = 'contactDiv';
 
 const slicingPizzaImg = document.createElement('img');
 slicingPizzaImg.src = slicingPizzaGif;
 
 //Icons
-const deliveryIcon = document.createElement('img').classList.add('icon');
-const phoneIcon = document.createElement('img').classList.add('icon');
-const emailIcon = document.createElement('img').classList.add('icon');
-const adressIcon = document.createElement('img').classList.add('icon');
+const deliveryIcon = document.createElement('img');
+const phoneIcon = document.createElement('img');
+const emailIcon = document.createElement('img');
+const adressIcon = document.createElement('img');
+
+deliveryIcon.classList.add('icon');
+phoneIcon.classList.add('icon');
+emailIcon.classList.add('icon');
+adressIcon.classList.add('icon');
 
 deliveryIcon.src = moped;
 phoneIcon.src = phone;
@@ -38,17 +44,30 @@ const phoneDiv = document.createElement('div');
 const emailDiv = document.createElement('div');
 const adressDiv = document.createElement('div');
 
+deliveryDiv.classList.add('infoContainer');
+deliveryDiv.id = 'deliveryDiv';
+phoneDiv.classList.add('infoContainer');
+emailDiv.classList.add('infoContainer');
+adressDiv.classList.add('infoContainer');
+//Contact container
+const contactContainer = document.createElement('div');
+contactContainer.id = 'contactContainer';
+//Append
 deliveryDiv.append(deliveryIcon, freeDeliveryMessage);
 phoneDiv.append(phoneIcon, phoneNumber);
 emailDiv.append(emailIcon, emailAdress);
 adressDiv.append(adressIcon, restaurantAdress);
 
-contactDiv.append(
-  slicingPizzaImg,
-  deliveryDiv,
+contactContainer.append(
   phoneDiv,
   emailDiv,
   adressDiv
+);
+
+contactDiv.append(
+  slicingPizzaImg,
+  deliveryDiv,
+  contactContainer
 );
 
 export {contactDiv};
