b78c98a82298c711fc516c59bd970e36ef7ac389
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-02-08 11:26:57 -0300

    Create de initial tab browse system

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import {homeDiv} from './modules/home.js';
+import {menuContainer} from './modules/menu.js';
 import {contactDiv} from './modules/contact.js';
 
 const content = document.querySelector('#content');
 
-content.appendChild(contactDiv);
+const homeTab = document.querySelector('#home');
+const menuTab = document.querySelector('#menu');
+const contactTab = document.querySelector('#contact');
+
+content.appendChild(homeDiv);
+
+homeTab.addEventListener('click', () => {
+  content.firstElementChild.remove()
+  content.appendChild(homeDiv);
+});
+menuTab.addEventListener('click', () => {
+  content.firstElementChild.remove()
+  content.appendChild(menuContainer);
+});
+contactTab.addEventListener('click', () => {
+  content.firstElementChild.remove();
+  content.appendChild(contactDiv);
+});
