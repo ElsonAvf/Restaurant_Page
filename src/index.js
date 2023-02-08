1a32a99e3c073d68e0d779ef312fcd9cc519cb3e
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-02-08 11:54:07 -0300

    Add touchmove event that change homeTab borders

--- src/index.js
+++ src/index.js
@@ -10,15 +10,40 @@ const contactTab = document.querySelector('#contact');
 
 content.appendChild(homeDiv);
 
+const removeHomeTabClass = () => {
+  if (homeTab.classList.contains('homeInitialStyle')) {
+    homeTab.classList.remove('homeInitialStyle');
+  };
+};
+
+//Hover Events
+menuTab.addEventListener('touchmove', (e) => {
+  const touch = e.touches[0];
+  const checkbox = document.elementFromPoint(touch.clientX, touch.clientY);
+  if (checkbox) {
+    removeHomeTabClass();
+  };
+});
+contactTab.addEventListener('touchmove', (e) => {
+  const touch = e.touches[0];
+  const checkbox = document.elementFromPoint(touch.clientX, touch.clientY);
+  if (checkbox) {
+    removeHomeTabClass();
+  };
+});
+//Click Events
 homeTab.addEventListener('click', () => {
+  removeHomeTabClass();
   content.firstElementChild.remove()
   content.appendChild(homeDiv);
 });
 menuTab.addEventListener('click', () => {
+  removeHomeTabClass();
   content.firstElementChild.remove()
   content.appendChild(menuContainer);
 });
 contactTab.addEventListener('click', () => {
+  removeHomeTabClass();
   content.firstElementChild.remove();
   content.appendChild(contactDiv);
 });
